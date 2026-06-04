22b7f63bac1163c4c20484475a8a4e236910c9a6
manisiva1988-maker <manisiva.1988@gmail.com>
2026-06-04 15:10:47 +0530

    Update tests/example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -25,10 +25,10 @@ test('Browser',async()=>{
     const context =await browser.newContext({
       recordVideo:{
         dir:'./screen/'
-      }
+      } 
     });
     const page =await context.newPage()
     await page.goto('https://www.amazon.in/')
     await page.waitForTimeout(6000)
-    
+    await page.waitForTimeout(3000)
 })
