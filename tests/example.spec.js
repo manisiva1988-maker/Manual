93c9990baa7755c0bf82898c164aa9cb947c2c10
manisiva1988-maker <manisiva.1988@gmail.com>
2026-07-09 15:26:09 +0530

    Prepare local changes for push

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,34 +1,20 @@
-// // @ts-check
+// @ts-check
 import { test, expect, chromium } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('Browser', async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext({
+    recordVideo: { dir: './screen/' }
+  });
+  const page = await context.newPage();
+  await page.goto('https://www.amazon.in/');
+  await page.waitForTimeout(4000);
+  await page.waitForTimeout(7000);
+});
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
-
-
-
-test('Browser',async()=>{
-    const browser =await chromium.launch()
-    const context =await browser.newContext({
-      recordVideo:{
-        dir:'./screen/'
-      } 
-    });
-    const page =await context.newPage()
-    await page.goto('https://www.amazon.in/')
-    await page.waitForTimeout(4000)
-    await page.waitForTimeout(7000)
-})
+test('Read JSON Response', async ({ request }) => {
+  const response = await request.get('https://reqres.in/api/users/2');
+  const body = await response.json();
+  console.log(body);
+});
+>>>>>>> 9cb503e (Prepare local changes for push)
